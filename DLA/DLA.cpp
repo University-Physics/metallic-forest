bd437c11c81ec29e304b063358b9bad3ea88b604
Ronalito <rscortessa@unal.edu.co>
2021-03-31 20:23:57 -0500

    Arreglo de la función casilla y condiciones de frontera cuando Na es múltiplo de 110

--- DLA/DLA.cpp
+++ DLA/DLA.cpp
@@ -4,7 +4,7 @@
 #include<algorithm>
 #include <cstdlib>
 std::vector<int> vecinos(int Na, std::vector <int> &a);
-int casilla(int N, std::vector <int> &b);
+int casilla(int N);
 void print(std::vector <int> &a);
 int main()
 {
@@ -21,7 +21,7 @@ int main()
     if(find(begin(b),end(b),1) != std::end(b)){ a[Na]=1; break;}
     /*Numero aleatorio de 0 a 4*/
     int random=rand() % 5; 
-    if( Na+casilla(rand() % 5,b)<a.size() && Na+casilla(rand() % 5,b)>0 && a[Na+casilla(rand() % 5,b)]!=1) Na=Na+casilla(rand() % 5,b);
+    if( Na+casilla(random)<a.size() && Na+casilla(random)>0 && b[random]!=2) Na=Na+casilla(random);
     else break;
     }
     }
@@ -38,13 +38,13 @@ std::vector<int> vecinos(int Na, std::vector <int> &a)
   else veci.push_back(2);
   if(Na-m>=0) veci.push_back(a[Na-m]);
   else veci.push_back(2);
-  if(Na+1<a.size()) veci.push_back(a[Na+1]);
+  if(Na+1<a.size() && (Na+1)%110!=0) veci.push_back(a[Na+1]);
   else veci.push_back(2);
-  if(Na-1>=0) veci.push_back(a[Na-1]);
+  if(Na-1>=0 && (Na)%110!=0) veci.push_back(a[Na-1]);
   else veci.push_back(2);
   return veci;
 }
-int casilla(int N, std::vector <int> &b)
+int casilla(int N)
 {
   int m=110;  
   if(N==0) return m;
