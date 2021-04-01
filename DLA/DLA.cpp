9116af31f8d8e37eb0b0ff1f437e64dac25c00e1
Ronalito <rscortessa@unal.edu.co>
2021-03-31 23:35:36 -0500

    Cambios en el código,en la  generación de números aleatorios para el paso de la partícula, se cambio rand()%5 por rand()%4 y se agrego a[6105] como sitio de la partícula semilla

--- DLA/DLA.cpp
+++ DLA/DLA.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<vector>
+#include<functional>
 #include<cmath>
+#include<random>
 #include<algorithm>
 #include <cstdlib>
 std::vector<int> vecinos(int Na, std::vector <int> &a);
@@ -9,18 +11,19 @@ void print(std::vector <int> &a);
 int main()
 {
   std::vector<int> a(12100,0);
-  a[6050]=1;
+  a[6105]=1;
   const int m=sqrt(a.size());
   for(int particula=0;particula<10000;particula++)
     {
-      int Na=rand() % 12100;
+   
+ int Na=rand() %12100;
       while(Na==60){  Na=rand() % 12100; }
   while(true)
     {
     std::vector<int> b=vecinos(Na,a);
     if(find(begin(b),end(b),1) != std::end(b)){ a[Na]=1; break;}
     /*Numero aleatorio de 0 a 4*/
-    int random=rand() % 5; 
+    int random=rand() % 4;
     if( Na+casilla(random)<a.size() && Na+casilla(random)>0 && b[random]!=2) Na=Na+casilla(random);
     else break;
     }
@@ -50,7 +53,7 @@ int casilla(int N)
   //Esta función retorna las casillas que debe recorrer la partícula en el vector, para ir a los sitios vecinos
   int m=110;  
   if(N==0) return m; // Número de sitios de casillas para ir al sitio de abajo
-  else if(N==1) return -m; // Número de sitios de casillas para ir al sitio de arriba
+  else if(N==1) return (-m); // Número de sitios de casillas para ir al sitio de arriba
   else if(N==2) return 1; // Número de sitios de casillas para ir al sitio de la derecha
   else return -1; // Número de sitios de casillas para ir al sitio de la izquierda
 }
