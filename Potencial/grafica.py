dab528f1db8a36e58c2fd45eb79786be8983f48f
Ronalito <rscortessa@unal.edu.co>
2021-05-11 11:33:49 -0500

    Se añade una forma de graficar el fractal

---
+++ Potencial/grafica.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+archivo=open("data.txt")
+A=archivo.read().split("\n")
+X=[]
+Y=[]
+Z=[]
+for line in A:
+
+    C=re.findall("[0-9]*",line)
+    if len(C) > 1:
+        X.append(int(C[0]))
+        Y.append(int(C[2]))
+        Z.append(int(C[4]))
+colors = (0,0,0)
+area = np.pi*3
+plt.set_cmap("Greys")
+plt.scatter(X, Y, s=area, c=Z, alpha=0.5)
+plt.title("DLA")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+plt.savefig("DLA.png")
