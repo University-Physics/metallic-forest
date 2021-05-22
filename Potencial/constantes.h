1292317e09fcb5792738bff9a350442044a8d84d
Federico Garcia <fegarciaga@unal.edu.co>
2021-05-22 17:27:05 -0500

    1

--- Potencial/constantes.h
+++ Potencial/constantes.h
@@ -1,9 +1,8 @@
-const double DELTA = 0.05;
+const double DELTA = 1./201;
 const double XMIN = 0.0;
-const double XMAX = 10;
+const double XMAX = 1;
 const double YMIN = 0.0;
-const double YMAX = 10;
+const double YMAX = 1;
 const int NX = (XMAX-XMIN)/DELTA;
 const int NY = (YMAX-YMIN)/DELTA;
 const int NSTEPS = 100;
-const int Nmax = 100;
