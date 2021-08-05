9570f034e1ee6062f8104fdf02ed59156c7b32e4
Ronalito <rscortessa@unal.edu.co>
2021-08-05 15:08:33 -0500

    Enviado

--- Potencial/fronteras.sh
+++ Potencial/fronteras.sh
@@ -1,10 +1,10 @@
 arg=(1 2 3 4 5 6 7 8 9 10)
 for V in ${arg[*]}; do
-    ./main.x 1 10 1 2 $V $1 &
+    ./main.x 1 50 1 2 $V $1 &
 done
 wait
 for S in ${arg[*]}; do
-    mv "./data/Out1T10V1R2I"$S"S.txt" "./data/fronteras/"$1"Out1T10V1R2I"$S"S.txt"
-    mv "1T10V1R2I"$S"SProbability_distribution.txt" "./data/fronteras/P/1T10V1R2I"$S"SProbability_distribution.txt"
+    mv "./data/Out1T50V1R2I"$S"S.txt" "./data/fronteras/"$1"Out1T50V1R2I"$S"S.txt"
+    mv "1T50V1R2I"$S"SProbability_distribution.txt" "./data/fronteras/P/1T50V1R2I"$S"SProbability_distribution.txt"
 done
 
